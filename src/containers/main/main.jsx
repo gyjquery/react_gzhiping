9d35a3662305cae5b4baba645e57c9b5f9581d29
gaoyang <gy1693850632@163.com>
2018-08-14 21:25:39 +0800

    Update main.jsx

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -4,7 +4,7 @@ export default class main extends Component{
   render(){
     return(
 
-       <Button type="primary"> dev4用户登录中心</Button>
+       <Button type="primary"> dev5用户登录中心</Button>
 
     )
   }
