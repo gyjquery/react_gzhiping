8f4dae1ca7541e3871a3ec4bc094e632dab453bf
gaoyang <1693850632@qq.com>
2018-08-14 20:57:24 +0800

    'dev'

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -4,7 +4,7 @@ export default class main extends Component{
   render(){
     return(
 
-       <Button type="primary"> 用户登录中心</Button>
+       <Button type="primary"> dev用户登录中心</Button>
 
     )
   }
